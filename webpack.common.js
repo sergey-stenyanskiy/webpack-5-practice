a46793cc3f7c54c8ee92c53fb646ec049d69d92b
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-25 12:41:47 +0300

    первоначальная настройка webpack

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(m|c)?jsx?$/i,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.pug$/i,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.s(a|c)ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|svg)$/i,
+        type: 'asset/inline'
+      },
+      {
+        test: /\.(png|gif|jpe?g)$/i,
+        type: 'asset/resource'
+      },
+    ]
+  }
+}
---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+const index = {
+  filename: 'index.html',
+  template: './src/template/index.pug',
+  chunks: ['index']
+}
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    historyApiFallback: {
+      index: '/'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin(index),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+});
