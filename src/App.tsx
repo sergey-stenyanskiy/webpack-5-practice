712db65e55f0c2a4d2cdc05d52ff105a37b333a7
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-29 10:45:03 +0300

    добавление изображения в соответствии с заданием

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import pizza from './assets/images/pizza.jpg'
+
 export default () => (
   <div className="app">
     Hello, world!
+    <img src={pizza} title="pizza" alt="pizza" />
   </div>
 )
